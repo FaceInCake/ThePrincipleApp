5d48f24dacb850211cb7b4abb670469cc3569559
Matthew <faceincake@gmail.com>
2022-04-01 21:15:34 -0400

    Updated activities and layouts

--- app/src/main/java/com/example/theprincipleapp/HomePage.java
+++ app/src/main/java/com/example/theprincipleapp/HomePage.java
@@ -1,8 +1,11 @@
 package com.example.theprincipleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
+
+import com.example.theprincipleapp.db.UserDatabase;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+        UserDatabase.createDatabase(getApplicationContext());
     }
+
 }
