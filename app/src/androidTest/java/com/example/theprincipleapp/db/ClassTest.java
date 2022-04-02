8bf4534d59562ca2a27bd704eedb5a7ad945797a
Matthew <faceincake@gmail.com>
2022-04-01 21:28:47 -0400

    ClassTest adjustment

--- app/src/androidTest/java/com/example/theprincipleapp/db/ClassTest.java
+++ app/src/androidTest/java/com/example/theprincipleapp/db/ClassTest.java
@@ -2,6 +2,7 @@ package com.example.theprincipleapp.db;
 
 import android.content.Context;
 
+import androidx.room.Room;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
@@ -13,15 +14,15 @@ import java.sql.SQLException;
 @RunWith(AndroidJUnit4.class)
 public class ClassTest extends TestCase {
     Context appCon;
+    UserDatabase udb;
 
     public void setUp() throws Exception {
         super.setUp();
         appCon = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        if (! UserDatabase.createDatabase(appCon))
-            throw new SQLException("Failed to create user-database");
+        udb = Room.inMemoryDatabaseBuilder(appCon, UserDatabase.class).build();
     }
 
     public void tearDown() {
-        UserDatabase.UDB.close();
+        udb.close();
     }
 }
