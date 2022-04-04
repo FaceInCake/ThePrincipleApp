2c0c41d37aeabe01cada9a61600a8727efa214f3
Matthew <faceincake@gmail.com>
2022-04-04 18:33:12 -0400

    Test modifications

--- app/src/androidTest/java/com/example/theprincipleapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/theprincipleapp/ExampleInstrumentedTest.java
@@ -2,25 +2,49 @@ package com.example.theprincipleapp;
 
 import android.content.Context;
 
+import androidx.room.Room;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import com.example.theprincipleapp.db.UserDatabase;
 
-import static org.junit.Assert.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
 
 /**
  * Instrumented test, which will execute on an Android device.
- *
+ * Also provided a setUp and tearDown method. Feel free to delete what you don't use.
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
+public class ExampleInstrumentedTest extends TestCase {
+    static Context appCon;
+    static UserDatabase udb;
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        appCon = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        udb = Room.inMemoryDatabaseBuilder(appCon, UserDatabase.class).build();
+    }
+
+    @AfterClass
+    public static void teardown() {
+        udb.close();
+    }
+
+    @Before
+    public void before() {
+        udb.clearAllTables();
+    }
+
     public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.theprincipleapp", appContext.getPackageName());
+        assertEquals("com.example.theprincipleapp", appCon.getPackageName());
+    }
+
+    public void useDatabase() {
+        assertTrue(udb.isOpen());
     }
 }
--- app/src/androidTest/java/com/example/theprincipleapp/db/TaskTest.java
+++ app/src/androidTest/java/com/example/theprincipleapp/db/TaskTest.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(AndroidJUnit4.class)
-public class ClassTest extends TestCase {
+public class TaskTest extends TestCase {
     Context appCon;
     UserDatabase udb;
 
