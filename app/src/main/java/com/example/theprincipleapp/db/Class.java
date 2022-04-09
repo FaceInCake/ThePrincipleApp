aae3fe4eab6302af7e1acf9996ed89b993467058
Matthew <faceincake@gmail.com>
2022-04-09 14:22:20 -0400

    year and term functions

--- app/src/main/java/com/example/theprincipleapp/db/Class.java
+++ app/src/main/java/com/example/theprincipleapp/db/Class.java
@@ -1,5 +1,7 @@
 package com.example.theprincipleapp.db;
 
+import android.util.Pair;
+
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Entity;
@@ -9,8 +11,8 @@ import androidx.room.Insert;
 import androidx.room.PrimaryKey;
 import androidx.room.Query;
 import androidx.room.Update;
+import java.util.Calendar;
 import java.util.Date;
-import io.reactivex.rxjava3.core.Completable;
 
 /**
  * Base Class Entity.
@@ -27,6 +29,49 @@ public class Class {
     public Date start;
     public Date end;
     public String professor = "TBD";
+
+    /**
+     * Gets the year of this class
+     * @return int year, ex. 2019
+     */
+    public int getYear () {
+        Calendar c = Calendar.getInstance();
+        c.setTime(start);
+        return c.get(Calendar.YEAR);
+    }
+
+    /**
+     * Gets the term of this class
+     * @return 0=Winter, 1=Summer, 2=Fall
+     */
+    public int getTerm() {
+        Calendar c = Calendar.getInstance();
+        c.setTime(start);
+        int m = c.get(Calendar.MONTH);
+        if (m <= 2) return 0;
+        if (m <= 6) return 1;
+        return 2;
+    }
+
+    /**
+     * Returns the unix min start and max end time of the given year and term
+     * @param year The int year in question
+     * @param term int repr of the term. 0=Winter, 1=Summer, 2=Fall
+     * @return First: minimum start date of term in unix time, Second: maximum end date of term in unix time
+     */
+    public static Pair<Long,Long> getTermBounds (int year, int term) throws IllegalArgumentException {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(0);
+        c.set(Calendar.YEAR, year);
+        long a = c.getTimeInMillis();
+        final long month = 2629743;
+        switch (term) {
+            case 0: return new Pair<>(a, a+month);
+            case 1: return new Pair<>(a+month*3, a+month*5);
+            case 2: return new Pair<>(a+month*7, a+month*9);
+            default: throw new IllegalArgumentException("term is out of range");
+        }
+    }
 
     @Dao
     public interface DAO {
