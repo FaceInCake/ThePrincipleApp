a6cef195a8ffc1f7d5eab694ed3e31801adbb520
Sophie <61323326+sophiep99@users.noreply.github.com>
2022-04-10 14:54:07 -0400

    New meeting layout

--- app/src/main/java/com/example/theprincipleapp/NewMeeting.java
+++ app/src/main/java/com/example/theprincipleapp/NewMeeting.java
@@ -3,12 +3,35 @@ package com.example.theprincipleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class NewMeeting extends AppCompatActivity {
+
+    Button btn_ok, btn_cancel;
+    Spinner spinnerMeetingType, spinnerWeekday;
+    EditText et_section, et_location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_meeting);
+
+        btn_ok = findViewById(R.id.btn_okmeeting);
+        btn_cancel = findViewById(R.id.btn_cancelmeeting);
+        spinnerMeetingType = findViewById(R.id.spinner_meetingtype);
+        spinnerWeekday = findViewById(R.id.spinner_meetingweekday);
+        et_section = findViewById(R.id.et_section);
+        et_location = findViewById(R.id.et_location);
+
+        btn_ok.setOnClickListener(view -> {
+
+        });
+
+        btn_cancel.setOnClickListener(view -> {
+
+        });
+
     }
 }
