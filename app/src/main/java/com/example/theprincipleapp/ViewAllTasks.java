1ddd5129682f91be8be0a7cad710f1ee2a58c409
Matthew <faceincake@gmail.com>
2022-04-10 13:01:07 -0400

    ViewAllTasks start

---
+++ app/src/main/java/com/example/theprincipleapp/ViewAllTasks.java
@@ -0,0 +1,14 @@
+package com.example.theprincipleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewAllTasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all_tasks);
+    }
+}
