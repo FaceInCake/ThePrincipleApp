f56c4148269c10eb012d0fa12514311b4479b6b3
Matthew <faceincake@gmail.com>
2022-04-10 16:54:07 -0400

    ViewClass fix and update

--- app/src/main/java/com/example/theprincipleapp/ViewAllTasks.java
+++ app/src/main/java/com/example/theprincipleapp/ViewAllTasks.java
@@ -1,8 +1,13 @@
 package com.example.theprincipleapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class ViewAllTasks extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class ViewAllTasks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_all_tasks);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_viewallclasses, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                Intent i = new Intent(this, NewTask.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
