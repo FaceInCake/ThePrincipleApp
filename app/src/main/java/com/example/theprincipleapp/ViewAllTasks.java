d82490862c94347f2a1b1cc0cd35d4b9c3c82649
Matthew <faceincake@gmail.com>
2022-04-10 18:08:44 -0400

    Viewall tasks

--- app/src/main/java/com/example/theprincipleapp/ViewAllTasks.java
+++ app/src/main/java/com/example/theprincipleapp/ViewAllTasks.java
@@ -2,19 +2,28 @@ package com.example.theprincipleapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.example.theprincipleapp.helpers.TaskAdapter;
 
 public class ViewAllTasks extends AppCompatActivity {
+    RecyclerView recView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_all_tasks);
+
+        recView = findViewById(R.id.vat_recView);
+        recView.setAdapter(new TaskAdapter(this));
     }
 
     @Override
