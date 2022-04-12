deaa1c0a555d86afb95653133bbef7131e5e2119
Matthew <faceincake@gmail.com>
2022-04-12 19:30:55 -0400

    NewClass has start and end date field

--- app/src/main/java/com/example/theprincipleapp/NewClass.java
+++ app/src/main/java/com/example/theprincipleapp/NewClass.java
@@ -2,9 +2,9 @@ package com.example.theprincipleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,12 +15,18 @@ import com.example.theprincipleapp.db.Task;
 import com.example.theprincipleapp.db.UserClass;
 import com.example.theprincipleapp.db.UserDatabase;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class NewClass extends AppCompatActivity {
-    EditText et_classCode, et_fullName, et_description, et_professor;
+    EditText et_classCode, et_fullName, et_description, et_professor, et_startDate, et_endDate;
+    Calendar c;
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd", Locale.getDefault());
+    Date start, end;
     Button button_ok, button_cancel;
 
     @Override
@@ -32,8 +38,18 @@ public class NewClass extends AppCompatActivity {
         et_fullName = findViewById(R.id.et_fullname);
         et_description = findViewById(R.id.et_description);
         et_professor = findViewById(R.id.et_professor);
+        et_startDate = findViewById(R.id.et_startDate);
+        et_endDate = findViewById(R.id.et_endDate);
         button_ok = findViewById(R.id.button_ok);
         button_cancel = findViewById(R.id.button_cancel);
+
+        c = Calendar.getInstance();
+        start = c.getTime();
+        c.roll(Calendar.MONTH, 4);
+        end = c.getTime();
+
+        et_startDate.setOnClickListener(view -> updateDate(start, et_startDate));
+        et_endDate.setOnClickListener(view -> updateDate(end, et_endDate));
 
         button_ok.setOnClickListener(view -> {
 
@@ -52,18 +68,34 @@ public class NewClass extends AppCompatActivity {
 
             Class c = new Class();
             c.professor = et_professor.getText().toString();
-            c.start = Calendar.getInstance().getTime();
-            c.end = Calendar.getInstance().getTime();
+            c.start = start;
+            c.end = end;
             userClass.cls = c;
 
             AsyncTask.execute(() -> {
                 UserDatabase.UDB.userClassDao().insert(userClass);
-                runOnUiThread(() -> {
-                    Toast.makeText(getApplicationContext(),"Course successfully added", Toast.LENGTH_LONG).show();
-                });
+                runOnUiThread(() ->
+                    Toast.makeText(getApplicationContext(),"Course successfully added", Toast.LENGTH_LONG).show()
+                );
             });
             finish();
         });
         button_cancel.setOnClickListener(view -> finish());
     }
+
+    public void updateDate (Date date, EditText et) {
+        c.setTime(date);
+        new DatePickerDialog(
+            this,
+            (datePicker, y, m, d) -> {
+                c.set(y, m, d);
+                date.setTime(c.getTime().getTime());
+                et.setText(sdf.format(date));
+            },
+            c.get(Calendar.YEAR),
+            c.get(Calendar.MONTH),
+            c.get(Calendar.DAY_OF_MONTH)
+        ).show();
+    }
+
 }
